3d2c6ceaa672ae37ef7a541fbecb2b9434cf5d65
Priyanshu Yadav <yadavpriyanshu9451@gmail.com>
2026-04-01 11:34:23 +0530

    Initial Commit

---
+++ SavePlate/SavePlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavePlateApp.swift
+//  SavePlate
+//
+//  Created by Priyanshu Yadav on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SavePlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
